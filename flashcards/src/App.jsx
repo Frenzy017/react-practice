f86a199628dae79e306af23a0b0d9871a1b4e6d9
Frenzy017 <aleksladjov4@gmail.com>
2023-08-28 20:20:06 +0300

    Update files

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,49 +1,35 @@
 import "./index.css";
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <div className="App">
-      <FlashCards />
-    </div>
-  );
+    return (<div className="App">
+        <FlashCards/>
+    </div>);
 }
 
-const questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript"
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components"
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props"
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook"
-  },
-  {
+const questions = [{
+    id: 3457, question: "What language is React based on?", answer: "JavaScript"
+}, {
+    id: 7336, question: "What are the building blocks of React apps?", answer: "Components"
+}, {
+    id: 8832, question: "What's the name of the syntax we use to describe a UI in React?", answer: "JSX"
+}, {
+    id: 1297, question: "How to pass data from parent to child components?", answer: "Props"
+}, {
+    id: 9103, question: "How to give components memory?", answer: "useState hook"
+}, {
     id: 2002,
-    question:
-      "What do we call an input element that is completely synchronised with state?",
+    question: "What do we call an input element that is completely synchronised with state?",
     answer: "Controlled element"
-  }
-];
+}];
 
 function FlashCards() {
-  return <div>
-    {questions.map(questions=>)}
-  </div>;
+    const [selectedId, setSelectedId] = useState(null);
+
+
+    return (<div className='flashcards'>
+        {questions.map((question) => (<div key={question.id}>
+            <p>{question.question}</p>
+        </div>))}
+    </div>);
 }
