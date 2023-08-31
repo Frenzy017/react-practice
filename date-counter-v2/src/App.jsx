9b5b4f30ed7702eac965959ad3ba6a57ca2f0b67
Frenzy017 <aleksladjov4@gmail.com>
2023-08-31 10:13:28 +0300

    Update files

--- date-counter-v2/src/App.jsx
+++ date-counter-v2/src/App.jsx
@@ -1,77 +1,47 @@
-/** @format */
-
 import {useState} from "react";
 import "./index.css";
 
 export default function App() {
-    return (
-        <div className="App">
-            <Counter/>
-        </div>
-    );
-
-
+    return (<div className="App">
+        <Counter/>
+    </div>);
 }
-console.log('test')
+
 function Counter() {
-
-    const [step, setStep] = useState(1);
     const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
 
-    const date = new Date();
+    function handleReset() {
+        setStep(0);
+        setCount(0);
+    }
+
+    const date = new Date("june 21 2027");
     date.setDate(date.getDate() + count);
 
-    function handlePrev() {
-        setStep((s) => s - 1);
-    }
-
-    function handleNext() {
-        setStep((s) => s + 1);
-    }
-
-    function countTrackerSubtract() {
-        setCount((c) => c - step);
-    }
-
-    function countTrackerPlus() {
-        setCount((c) => c + step);
-    }
-
-    return (
+    return (<div>
         <div>
-            <div style={{display: "flex", justifyContent: "center", gap: "5px"}}>
-                <input type='range' min='0' max='10'/>
-
-                <button style={{height: "25px", width: "25px"}} onClick={handlePrev}>
-                    -
-                </button>
-                <div>Step: {step}</div>
-                <button style={{height: "25px", width: "25px"}} onClick={handleNext}>
-                    +
-                </button>
-
-            </div>
-
-            <div style={{display: "flex", justifyContent: "center", gap: "5px"}}>
-                <button style={{height: "25px", width: "25px"}} onClick={countTrackerSubtract}>
-                    -
-                </button>
-                <div>Count: {count}</div>
-                <button style={{height: "25px", width: "25px"}} onClick={countTrackerPlus}>
-                    +
-                </button>
-            </div>
-
-            <span>
-        {count === 0 ? " Today is " : count > 0 ? ` ${count} days from today is ` : ` ${count} days ago was `}
-      </span>
-            <span>{date.toDateString()}</span>
+            <input type='range' min='0' max='10' value={step} onChange={e => setStep(Number(e.target.value))}/>
+            <span>Step: {step}</span>
+            <button onClick={() => setStep((c) => c + 1)}>+</button>
         </div>
-    );
-}
 
-// function addDays(date, days) {
-//   let result = new Date(date);
-//   result.setDate(result.getDate() + days);
-//   return result;
-// }
+        <div>
+            <button onClick={() => setCount((c) => c - step)}>-</button>
+            <input type='text' value={count} onChange={e => setCount(Number(e.target.value))}/>
+            <button onClick={() => setCount((c) => c + step)}>+</button>
+        </div>
+
+        <p>
+        <span>
+          {count === 0 ? "Today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}
+        </span>
+            <span>{date.toDateString()}</span>
+        </p>
+
+        {(count !== 0 || step !== 1 ? < div>
+            < button onClick={handleReset}>Reset</button>
+        </div> : null)}
+
+    </div>);
+}
